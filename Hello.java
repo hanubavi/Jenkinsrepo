a18a27dfba55e8642a4cb80c429d766faf3daa2a
hanubavi <61379290+hanubavi@users.noreply.github.com>
2020-03-06 14:16:28 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(sting[] args) {
   //Prints "Hello, World" in the terminal window.
-  system.out.printIn("Hello, World");
+  System.out.printIn("Hello, World");
   }
 }
